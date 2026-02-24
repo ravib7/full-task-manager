3f98e72d3f117664391fd0e6406ac4a5e8da3a23
ravib7 <ravibchaudhari7@gmail.com>
2026-02-24 08:54:47 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+require("dotenv").config()
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_URL)
+
+app.use("/", (req, res) => [
+    res.status(200).json({ message: "Task Manager Api Running..." })
+])
+
+mongoose.connection.once("open", () => {
+    console.log("db connected")
+    app.listen(process.env.PORT, () => {
+        console.log("server running")
+        console.log(`node: ${process.env.NODE_ENV}`)
+    })
+})
+
+module.exports = app
